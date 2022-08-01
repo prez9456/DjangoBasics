16b69dedd7b6b95045d2f5ba7b84a668784e69fb
prez9456 <57645205+prez9456@users.noreply.github.com>
2022-08-01 12:21:01 -0600

    init commit

---
+++ DjangoAPI/EmployeeApp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
+
+from EmployeeApp.models import Departments, Employees
+from EmployeeApp.serializers import DepartmentSerializer, EmployeesSerializer
+
+# Create your views here.
+@csrf_exempt
+def deparmentAPI(request, id=0):
+    if request.method == 'GET':
+        departments = Departments.objects.all()
+        departments_serializer = DepartmentSerializer(departments, many=True)
+        return JsonResponse(departments_serializer.data, safe=False)
